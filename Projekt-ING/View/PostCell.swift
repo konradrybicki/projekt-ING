8fed7f2ea28e448f5717865389bdf58d2a2a5e21
konradrybicki <konradrybicki.altamoda@gmail.com>
2020-05-09 10:14:17 +0200

    Rozpoczęta implementacja interfejsu dla postów

---
+++ Projekt-ING/View/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Projekt-ING
+//
+//  Created by Konrad Rybicki on 09/05/2020.
+//  Copyright © 2020 Konrad Rybicki. All rights reserved.
+//
+
+import UIKit
+
+//(na marginesie - Cocoatouch Class - klasa odpowiadająca komórkom tableView)
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() { //nib - stara nazwa dla xib
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
