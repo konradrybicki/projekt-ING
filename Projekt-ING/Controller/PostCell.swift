dcf6313ba2769e0a844b792d80ae4a2d5f37ae26
konradrybicki <konradrybicki.altamoda@gmail.com>
2020-05-18 15:36:23 +0200

    PostCell.swift - widok -> controller

--- Projekt-ING/Controller/PostCell.swift
+++ Projekt-ING/Controller/PostCell.swift
@@ -26,7 +26,7 @@ class PostCell: UITableViewCell {
         
         //..
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
